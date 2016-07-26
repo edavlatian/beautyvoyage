abed4bae238627e54c08580bd647cbc5bbff55fa
edavlatian <chaosmage89+git@gmail.com>
2016-07-26 16:40:34 -0400

    Old Business Website contents for Beauty Voyage Salon and Spa
    Final version of the website after business closed.

---
+++ scripts/popup.js
@@ -0,0 +1,6 @@
+﻿function popUp(URL, wLen, hLen) {
+	day = new Date();
+	id = day.getTime();
+	eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width='+wLen+',height='+hLen+',left = 20,top = 20');");
+}
+
